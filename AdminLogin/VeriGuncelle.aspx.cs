036b26d01c89beeab33d4483286d03b071be0c76
GÖKALP <gokalppabdullah@gmail.com>
2024-08-23 16:19:51 +0300

    AdminLogin

---
+++ AdminLogin/VeriGuncelle.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdminLogin
+{
+	public partial class VeriGuncelle : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			int x = Convert.ToInt32(Request.QueryString["ID"].ToString());
+			DataSet1TableAdapters.TBLADMINTableAdapter dt = new DataSet1TableAdapters.TBLADMINTableAdapter();
+			TextBox1.Text = x.ToString();
+			TextBox1.Enabled = false;
+
+			if (Page.IsPostBack == false)
+			{
+				TextBox2.Text = dt.VeriGetir(Convert.ToInt32(TextBox1.Text))[0].KULLANICI;
+				TextBox3.Text = dt.VeriGetir(Convert.ToInt32(TextBox1.Text))[0].SIFRE;
+				TextBox4.Text = dt.VeriGetir(Convert.ToInt32(TextBox1.Text))[0].SIFRE;
+			}
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			DataSet1TableAdapters.TBLADMINTableAdapter dt = new DataSet1TableAdapters.TBLADMINTableAdapter();
+			dt.AdminGuncelle(TextBox2.Text, TextBox3.Text, Convert.ToInt32(TextBox1.Text));
+			Response.Redirect("Veriler.Aspx");
+		}
+	}
+}
---
+++ AdminLogin/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AdminLogin
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+		}
+
+		protected void Button1_Click(object sender, EventArgs e)
+		{
+			string connectionString = @"Data Source=GÖKALP\SQLEXPRESS;Initial Catalog=AdminDB;Integrated Security=True";
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					connection.Open();
+					string query = "SELECT * FROM TBLADMIN WHERE KULLANICI=@P1 AND SIFRE=@P2";
+					using (SqlCommand command = new SqlCommand(query, connection))
+					{
+						command.Parameters.AddWithValue("@P1", TextBox1.Text);
+						command.Parameters.AddWithValue("@P2", TextBox2.Text);
+
+						SqlDataReader reader = command.ExecuteReader();
+
+						if (reader.Read())
+						{
+							Response.Redirect("Veriler.aspx");
+						}
+						else
+						{
+							Response.Write("Hatalı Veri Girişi");
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					// Hata mesajı yazdırma veya loglama
+					Response.Write("Bir hata oluştu: " + ex.Message);
+				}
+			}
+		}
+	}
+}
